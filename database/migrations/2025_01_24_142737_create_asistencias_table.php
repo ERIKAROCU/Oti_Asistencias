01c5f08a783f8a337608d1a5ac846b6af8965ca6
BrayanErik <unam11erik@gmail.com>
2025-01-24 09:31:38 -0500

    Proyecto con la tabla de empleados

---
+++ database/migrations/2025_01_24_142737_create_asistencias_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('asistencias', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('empleado_id')->constrained()->onDelete('cascade'); // Relación con empleados
+        $table->timestamp('entrada')->nullable();
+        $table->timestamp('salida')->nullable();
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('asistencias');
+    }
+};
